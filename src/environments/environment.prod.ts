085fca3b9c2d5fa104391b1d2bf2a7a27a3fce83
Shijith Kandirithy Mohanan <kandirithy.m@pg.com>
2019-05-19 22:27:21 +0530

    audit fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCye_nC8c9v0vtCPJcGppH01sgRZ2lzzVU',
+    authDomain: 'lmsapp-4a7c5.firebaseapp.com',
+    databaseURL: 'https://lmsapp-4a7c5.firebaseio.com',
+    projectId: 'lmsapp-4a7c5',
+    storageBucket: 'lmsapp-4a7c5.appspot.com',
+    messagingSenderId: '249237162514'
+  },
+  googleBookAPI: 'https://www.googleapis.com/books/v1/volumes'
 };
